8938a9a2b4434fb32838049384070d1c6ef5ca22
Aleksey Koshkin <koshkin.pydev@mail.com>
2023-06-17 08:48:57 +0300

    Add router

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
 
-type myHandler struct {
-}
-
-func (myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
-}
+//func index(w http.ResponseWriter, r *http.Request) {
+//	w.Write([]byte("Home"))
+//}
+//func hello(w http.ResponseWriter, r *http.Request) {
+//	name := strings.Split(r.URL.Path, "/")[2]
+//	w.Write([]byte(fmt.Sprintf("Hello, %s", name)))
+//}
 
 func main() {
-	err := http.ListenAndServe(":3000", myHandler{})
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("index page"))
+	})
+
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		panic(err)
 	}
